761e17b9c6c0cc0b8c200da719b860e27f5e79ef
mitchellf4 <30480749+mitchellf4@users.noreply.github.com>
2017-11-16 18:25:55 -0500

    Add files via upload

---
+++ main/java/com/example/mfinstad/app_example1/SignIn.java
@@ -0,0 +1,14 @@
+package com.example.mfinstad.app_example1;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SignIn extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+
+}
---
+++ main/java/com/example/mfinstad/app_example1/SignUp.java
@@ -0,0 +1,16 @@
+package com.example.mfinstad.app_example1;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SignUp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+
+
+}
